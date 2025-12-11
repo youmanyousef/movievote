c0b077bfa8da7096ca7aa9adc7df08679b9fc632
Joseph Altareb <youmanyousef@gmail.com>
2025-12-11 03:30:44 -0800

    update existing pages with partial templates

--- routes/vote.js
+++ routes/vote.js
@@ -6,35 +6,59 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	console.log('hi');
-    res.render('vote/home');
+    res.render('vote/home', { 
+		title: 'Home',
+		message: 'Welcome to the Authentication Template'
+	});
 });
 
 router.get('/create', (req, res) => {
-    res.render('vote/create');
+    res.render('vote/create', { 
+		title: 'Create',
+		message: 'Welcome to the Authentication Template'
+	});
 });
 
 router.get('/join', (req, res) => {
-    res.render('vote/join');
+    res.render('vote/join', { 
+		title: 'Join',
+		message: 'Welcome to the Authentication Template'
+	});
 });
 
 router.get('/lobby', (req, res) => {
-    res.render('vote/lobby');
+    res.render('vote/lobby', { 
+		title: 'Lobby',
+		message: 'Welcome to the Authentication Template'
+	});
 });
 
 router.get('/vote', (req, res) => {
-    res.render('vote/vote');
+    res.render('vote/vote', { 
+		title: 'Vote',
+		message: 'Welcome to the Authentication Template'
+	});
 });
 
-router.get('/results', (req, res) => {
-    res.render('vote/results');
+router.get('/result', (req, res) => {
+    res.render('vote/result', { 
+		title: 'Results',
+		message: 'Welcome to the Authentication Template'
+	});	
 });
 
 router.get('/choices', (req, res) => {
-    res.render('vote/choices');
+	res.render('vote/choices', { 
+		title: 'Choose',
+		message: 'Welcome to the Authentication Template'
+	});
 });
 
 router.get('/wait', (req, res) => {
-    res.render('vote/wait');
+	res.render('vote/wait', { 
+		title: 'Waiting...',
+		message: 'Welcome to the Authentication Template'
+	});
 }); 
 
 module.exports = router;
